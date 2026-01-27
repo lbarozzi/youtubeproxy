51a75bf422e6fb5a525132cb6feba97bbc258f7f
Leonardo Barozzi <leonardo.barozzi@gmail.com>
2026-01-27 10:13:05 +0100

    CORS

--- src/main/java/efohum/com/youtubeproxy/controller/YouTubeProxyController.java
+++ src/main/java/efohum/com/youtubeproxy/controller/YouTubeProxyController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,6 +26,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/youtube/v3")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin(origins = "*")
 public class YouTubeProxyController {
     
     private final YouTubeProxyService youTubeProxyService;
